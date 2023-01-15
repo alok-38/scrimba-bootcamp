6379050e5c28116ba04c72a12f14262ce722482e
alok <plantsfortrees@gmail.com>
2023-01-15 06:22:44 +0530

    Passenger Counter App

--- passenger-counter/app.js
+++ passenger-counter/app.js
@@ -1,7 +1,12 @@
-let count = document.getElementById("count-el");
+let countEl = document.getElementById("count-el");
 const incrementBtn = document.getElementById("increment-btn");
 const saveBtn = document.getElementById("save-btn");
+let count = 0;
 
 incrementBtn.addEventListener("click", (event) => {
-  count.innerHTML = count++;
+  countEl.innerText = count++;
 });
+
+saveBtn.addEventListener('click', (event) => {
+    
+})
