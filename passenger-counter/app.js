a6497dd831652f4e8ad62f9bd51dd2297c43dc56
alok <plantsfortrees@gmail.com>
2023-01-15 12:22:30 +0530

    Completed the app

--- passenger-counter/app.js
+++ passenger-counter/app.js
@@ -1,12 +1,27 @@
-let countEl = document.getElementById("count-el");
+// initialize count and total variable
+let count = 0;
+let total = 0;
+
+// grab all the DOM elements
+const countEl = document.getElementById("count-el");
+const saveEl = document.getElementById("save-el");
 const incrementBtn = document.getElementById("increment-btn");
 const saveBtn = document.getElementById("save-btn");
-let count = 0;
+const totalEl = document.getElementById("total-el");
 
 incrementBtn.addEventListener("click", (event) => {
-  countEl.innerText = count++;
+  count++;
+  countEl.textContent = count;
 });
 
-saveBtn.addEventListener('click', (event) => {
-    countEl.innerText = 0;
-})
+saveBtn.addEventListener("click", (event) => {
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+
+  total += count;
+  totalEl.textContent = total;
+
+  // reset counter variable and HTML after saving
+  countEl.textContent = 0;
+  count = 0;
+});
