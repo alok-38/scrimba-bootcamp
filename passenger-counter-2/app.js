a2b247ee48a358ca231193107a3542ba5dae5263
alok <plantsfortrees@gmail.com>
2023-01-16 10:46:31 +0530

    app.js

--- passenger-counter-2/app.js
+++ passenger-counter-2/app.js
@@ -13,21 +13,28 @@ const h3El1 = document.createElement("h3");
 // and give it some content
 const content = document.createTextNode("saved entries:" + " ");
 const content1 = document.createTextNode("Total: " + " ");
-// add the text node to the newly created div
+// add the text node to the newly created elements
 h3El.appendChild(content);
+h3El.setAttribute("id", "save");
 h3El1.appendChild(content1);
+h3El1.setAttribute("id", "total");
+
 // add the newly created element and its content into the DOM
-const buttonEl = document.getElementById("save-btn");
-const buttonEl1 = document.getElementById("increment-btn");
+const buttonEl1 = document.getElementById("save-btn");
+const buttonEl = document.getElementById("increment-btn");
 // Insert p below the buttons
 insertAfter(buttonEl, h3El);
 // Insert below the saved entries
-insertAfter(content, h3El1);
+insertAfter(buttonEl1, h3El1);
 
 // Add event listener to both increment and save buttons
-buttonEl1.addEventListener("click", (event) => {
+buttonEl.addEventListener("click", (event) => {
   count++;
   countEl.innerText = count;
 });
 
-buttonEl1.addEventListener("click", (event) => {});
+buttonEl1.addEventListener("click", (event) => {
+  h3El.innerText += " " + " " + " " + count + "-" + "-";
+  count = 0;
+  countEl.innerText = count;
+});
