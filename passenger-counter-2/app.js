f21d80b0d830f6e52bac3cfb23eb7b503bbf2662
alok <plantsfortrees@gmail.com>
2023-01-16 09:21:10 +0530

    Javascript

--- passenger-counter-2/app.js
+++ passenger-counter-2/app.js
@@ -2,14 +2,32 @@ function insertAfter(referenceNode, newNode) {
   referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
-// Create a new para element
-const pEl = document.createElement("p");
+// Querieng DOM elements
+const countEl = document.getElementById("count-el");
+// Initialise a variable for counting
+let count = 0;
+
+// Create two h3's
+const h3El = document.createElement("h3");
+const h3El1 = document.createElement("h3");
 // and give it some content
 const content = document.createTextNode("saved entries:" + " ");
+const content1 = document.createTextNode("Total: " + " ");
 // add the text node to the newly created div
-pEl.appendChild(content);
+h3El.appendChild(content);
+h3El1.appendChild(content1);
 // add the newly created element and its content into the DOM
 const buttonEl = document.getElementById("save-btn");
 const buttonEl1 = document.getElementById("increment-btn");
 // Insert p below the buttons
-insertAfter(buttonEl, pEl);
+insertAfter(buttonEl, h3El);
+// Insert below the saved entries
+insertAfter(content, h3El1);
+
+// Add event listener to both increment and save buttons
+buttonEl1.addEventListener("click", (event) => {
+  count++;
+  countEl.innerText = count;
+});
+
+buttonEl1.addEventListener("click", (event) => {});
