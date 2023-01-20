0915f8f039ea79d74f28f6268bbd850d16480926
alok <plantsfortrees@gmail.com>
2023-01-20 13:05:23 +0530

    Sorting Fruits

---
+++ emoji-fighter/app.js
@@ -0,0 +1,34 @@
+let fighters = [
+  "🐉",
+  "🐥",
+  "🐊",
+  "💩",
+  "🦍",
+  "🐢",
+  "🐩",
+  "🦭",
+  "🦀",
+  "🐝",
+  "🤖",
+  "🐘",
+  "🐸",
+  "🕷",
+  "🐆",
+  "🦕",
+  "🦁",
+];
+
+let stageEl = document.getElementById("stage");
+let fightButton = document.getElementById("fightButton");
+
+fightButton.addEventListener("click", function () {
+  // Challenge:
+  // When the user clicks on the "Pick Fighters" button, pick two random
+  // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
+  const numberOfFighters = 2;
+  const shuffledFighters = fighters.sort(() => 0.5 - Math.random());
+  stageEl.textContent =
+    shuffledFighters.slice(0, numberOfFighters - 1) +
+    "vs" +
+    shuffledFighters.slice(0, numberOfFighters * Math.random());
+});
