46353655f95213926e0f45cbfd02794c074c3965
alok <plantsfortrees@gmail.com>
2023-01-24 08:10:18 +0530

    Chrome Extention

---
+++ chrome-extention/app.js
@@ -0,0 +1,12 @@
+// Create two variables:
+// myLeads -> should be assigned to an empty array
+// inputEl -> should be assigned to the text input field
+
+const myLeads = [];
+const inputBtnEl = document.getElementById("input-btn");
+
+// Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
+
+inputBtnEl.addEventListener("click", (event) => {
+  console.log("Button clicked!");
+});
